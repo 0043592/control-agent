825d366332af271235192d71d3da5bc54fed7a3c
u <u@u.co>
2022-06-28 22:49:04 +0200

    v2 split docker build and docker run - add NODE_ENV variable

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ fi
 decrypted=$(echo "$1" | base64 --decode)
 IFS=';' read control_agent_settings SOCKD_USER_NAME SOCKD_USER_PASSWORD <<<$decrypted
 
-printf "CONTROL_AGENT_SETTINGS=$control_agent_settings\nPROXY_LOGIN=$SOCKD_USER_NAME\nPROXY_PASSWORD=$SOCKD_USER_PASSWORD\n" >.env
+printf "NODE_ENV=production\nCONTROL_AGENT_SETTINGS=$control_agent_settings\nPROXY_LOGIN=$SOCKD_USER_NAME\nPROXY_PASSWORD=$SOCKD_USER_PASSWORD\n" >.env
 distribution=$(lsb_release --id)
 version=$(lsb_release -cs)
 # ubuntu Setup
@@ -20,7 +20,7 @@ else
   echo "Install for system $distribution, $version is not supported"
   exit 1
 fi
-sudo apt update && sudo apt install --no-install-recommends --no-install-suggests -y apt-transport-https ca-certificates curl gnupg lsb-release && apt autoremove --purge -y apache2 nginx
+sudo apt update && sudo apt install --no-install-recommends --no-install-suggests -y make apt-transport-https ca-certificates curl gnupg lsb-release && apt autoremove --purge -y apache2 nginx
 
 # https://docs.docker.com/engine/install
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
